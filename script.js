e532e44bec549e40b05bc10dd74a10f47d62245a
Miguel Alves Pires De Oliveira <miguel0439@DIGITALCOLLEGE.LOCAL>
2024-08-29 21:21:25 -0300

    inserindo os campos com o cep

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+//console.log('js link');
 const cep = document.querySelector('#cep');
+const numero = document.querySelector('#numero');
 
 const consultaCep = async () => {
-    let cepValue = cep.value;
-    console.log(cep.value);
+  let cepValue = cep.value;
+  console.log(cepValue);
 
-    if (cepValue.length === 8) {
-        try {
-            const response =
-                await axios.get(`https://brasilapi.com.br/api/cep/v2/${cepValue}`);
-            console.log(response.data);
-        } catch (error) {
-            console.error(error);
-        }
+  if (cepValue.length === 8) {
+    try {
+      const response = await axios.get(`https://brasilapi.com.br/api/cep/v2/${cepValue}`);
+      console.log(response.data);
+
+      preencherCampos(response.data);
+      numero.focus();
+
+    } catch (error) {
+      console.error(error);
     }
+  }
+}
+
+const preencherCampos = data => {
+  const rua = document.querySelector('#rua');
+  const bairro = document.querySelector('#bairro');
+  const estado = document.querySelector('#estado');
+
+  rua.value = data.street;
+  bairro.value = data.neighborhood;
+  estado.value = data.state;
 
 }
 
-// quando fizer response pegar referencia dos outros cantos e ir setando
 //consultaCep('60420670')
